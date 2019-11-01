e9eedf8290ebef623704d9cdf0d3350e1344fb20
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-10-31 22:41:25 -0400

    now connected to db, create schema for User module, added validation on users registration api call

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please provide a Name ")
+      .not()
+      .isEmpty(),
+    check("email", "Please provide a valid Email").isEmail(),
+    check(
+      "password",
+      "Please enter a Password with at least 8 characters"
+    ).isLength({ min: 8 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Passed: " + JSON.stringify(req.body));
+  }
+);
 
 module.exports = router;
