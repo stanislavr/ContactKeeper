cb6f40636303af63b31c85e673166872f5d92c73
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-10-31 22:55:28 -0400

    can store registered user info with hashed pwd in the db

--- models/User.js
+++ models/User.js
@@ -16,7 +16,7 @@ const UserSchema = mongoose.Schema({
   },
   date: {
     type: Date,
-    required: Date.now
+    default: Date.now()
   }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -19,12 +20,38 @@ router.post(
       "Please enter a Password with at least 8 characters"
     ).isLength({ min: 8 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("Passed: " + JSON.stringify(req.body));
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "This email is already taken" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
