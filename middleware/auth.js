0326b2b33e5a8ec7c4892f923cd344b30e6c7267
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-11-06 23:22:57 -0500

    can get all user contacts

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,16 +5,16 @@ module.exports = function(req, res, next) {
   // get token from header
   const token = req.header("x-auth-token");
 
-  // check if not token
+  // check if token not provided
   if (!token) {
     return res
       .status(401)
       .json({ msg: "No token provided, authorization denied!" });
   }
 
+  // verify the token
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
-
     req.user = decoded.user;
     next();
   } catch (err) {
