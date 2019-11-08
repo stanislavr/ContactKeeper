88e9570502558331caa6fd231ec5f337fa76dc9c
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-11-08 00:37:04 -0500

    added context, can populate hardcoded contact items

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from "../types";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "stan",
+        email: "stan@dsa.com",
+        phone: "1111",
+        type: "personal"
+      },
+      {
+        id: 2,
+        name: "ana",
+        email: "ana@dsa.com",
+        phone: "22222",
+        type: "personal"
+      },
+      {
+        id: 3,
+        name: "Dan",
+        email: "dan@dsa.com",
+        phone: "33333",
+        type: "professional"
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
