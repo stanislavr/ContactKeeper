87598e2889156306e41c93c655404bafdede54d8
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-11-09 23:43:37 -0500

    can filter contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,14 +5,22 @@ import ContactContext from "../../context/contact/contactContext";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
 
   return (
     <div>
       <Fragment>
-        {contacts.map(contact => (
-          <ContactItem key={contact.id} contact={contact} />
-        ))}
+        {filtered
+          ? filtered.map(contact => (
+              <ContactItem key={contact.id} contact={contact} />
+            ))
+          : contacts.map(contact => (
+              <ContactItem key={contact.id} contact={contact} />
+            ))}
       </Fragment>
     </div>
   );
