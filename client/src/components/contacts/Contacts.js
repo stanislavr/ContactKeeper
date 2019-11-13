7fc37f791d6be14669a2d19cca6f8d9d947c6a70
“[Stanislav <[stan.rashevsky@gmail.com]>
2019-11-13 00:19:35 -0500

    can add a contact using api and ui

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -17,13 +17,13 @@ const Contacts = () => {
       <TransitionGroup>
         {filtered
           ? filtered.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames="item">
-                <ContactItem key={contact.id} contact={contact} />
+              <CSSTransition key={contact._id} timeout={500} classNames="item">
+                <ContactItem key={contact._id} contact={contact} />
               </CSSTransition>
             ))
           : contacts.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames="item">
-                <ContactItem key={contact.id} contact={contact} />
+              <CSSTransition key={contact._id} timeout={500} classNames="item">
+                <ContactItem key={contact._id} contact={contact} />
               </CSSTransition>
             ))}
       </TransitionGroup>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import axios from "axios";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -9,44 +9,34 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
+  CONTACT_ERROR
 } from "../types";
 
 const ContactState = props => {
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        name: "stan",
-        email: "stan@dsa.com",
-        phone: "1111",
-        type: "personal"
-      },
-      {
-        id: 2,
-        name: "ana",
-        email: "ana@dsa.com",
-        phone: "22222",
-        type: "personal"
-      },
-      {
-        id: 3,
-        name: "Dan",
-        email: "dan@dsa.com",
-        phone: "33333",
-        type: "professional"
-      }
-    ],
+    contacts: [],
     current: null,
-    filtered: null
+    filtered: null,
+    error: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
-  const addContact = contact => {
-    contact.id = uuid.v4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const addContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.post("/api/contacts", contact, config);
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   // Delete Contact
@@ -85,6 +75,7 @@ const ContactState = props => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
